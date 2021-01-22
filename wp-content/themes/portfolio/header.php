6a4274cfa15a2dbc54481d0582e6bda09e3e1a1d
LAPTOP-RP15FKKQ\kenta <kentaros19940329@gmail.com>
2021-01-23 01:23:08 +0900

    test

--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -33,7 +33,7 @@
         </div>
         <div class="header_nav">
           <ul class="list">
-            <li><a href="<?php home_url(); ?>/about">About</a></li>
+            <li><a href="<?php home_url(); ?>/about">Abouta</a></li>
             <li><a href="<?php home_url(); ?>/works">Works</a></li>
             <li><a href="<?php home_url(); ?>/contact">Contact</a></li>
           </ul>
